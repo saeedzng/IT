46ca8ac7bc971ae38763205b6be54e16f9964ea4
Saeid <saeed.zng@gmail.com>
2024-11-25 23:04:03 +0330

    add supabase

--- src/App.tsx
+++ src/App.tsx
@@ -230,25 +230,28 @@ function App() {
   }
   const connectionRestored = useIsConnectionRestored();
 
+  interface DataItem { id: number; LeftID: number; RightID: number; }
 
-
-    const [data, setData] = useState<any[]>([]);
-
+    const [data, setData] = useState<DataItem[]>([]);
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
       const fetchData = async () => {
         const { data, error } = await supabase
           .from('Users')
           .select('*');
+          console.log('Fetched data:', data);
 
         if (error) {
-          console.error(error);
+          console.error('Error fetching data:', error);
         } else {
           setData(data);
         }
+        setLoading(false);
       };
 
       fetchData();
     }, []);
+    if (loading) return <div>Loading...</div>;
 
 
     return (
@@ -273,14 +276,7 @@ function App() {
         <div className="down-section" >
           {[page_n === 0 && (
             <div>
-              <div>
-                <h1>Data from Supabase</h1>
-                <ul>
-                  {data.map((item) => (
-                    <li key={item.id}>{item.name}</li>
-                  ))}
-                </ul>
-              </div>
+<div> <h1>Data from Supabase</h1> <ul> {data.map((row) => ( <li key={row.id}>{row.RightID}</li>  ))} </ul> </div>
               <div>
                 <div>Modal state: {state?.status}</div>
                 <button onClick={open}>Open-modal</button>
