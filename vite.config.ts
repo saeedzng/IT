21093ac5c366b3f262d20a27aedecbb2b15fd95a
Saeid <saeed.zng@gmail.com>
2024-11-26 15:31:10 +0330

    add supabase

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+console.log('Vite Config:');
+console.log('VITE_SUPABASE_URL:', process.env.VITE_SUPABASE_URL);
+console.log('VITE_SUPABASE_API_KEY:', process.env.VITE_SUPABASE_API_KEY);
+
 export default defineConfig({
   plugins: [react()],
   base: '/IT/',
