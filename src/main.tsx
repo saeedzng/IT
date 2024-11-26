2bfad8b4fccbb6079a3f4ebefe29b95c0b096f0d
Saeid <saeed.zng@gmail.com>
2024-11-26 16:12:41 +0330

    add supabase

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,6 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react';
 // this manifest is used temporarily for development purposes
 const manifestUrl = 'https://raw.githubusercontent.com/ChickenFarmLand/chickenFarm/gh-pages/tonconnect-manifest.json';
 
-console.log('Environment Variables:'); 
-console.log('VITE_SUPABASE_URL:', import.meta.env.VITE_SUPABASE_URL); 
-console.log('VITE_SUPABASE_API_KEY:', import.meta.env.VITE_SUPABASE_API_KEY);
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
