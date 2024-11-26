9c1621353a87e39df0d4a653c0a61cfbed54ff5a
Saeid <saeed.zng@gmail.com>
2024-11-26 15:40:44 +0330

    add supabase

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -3,6 +3,9 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
 
+console.log('Supabase URL:', supabaseUrl);
+console.log('Supabase API Key:', supabaseKey);
+
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('supabaseUrl and supabaseKey are required.');
 }
