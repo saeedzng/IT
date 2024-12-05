97b083fc1114be911e5a0754054f43d50d348dda
Saeid <saeed.zng@gmail.com>
2024-12-05 17:06:08 +0330

    go to new plan

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://github.com/saeedzng/IT/blob/gh-pages/tonconnect-manifest.json';
+const manifestUrl = 'https://raw.githubusercontent.com/saeedzng/IT/blob/gh-pages/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
