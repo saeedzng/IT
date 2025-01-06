e33704fffe9cfc60adc34c5bca66e2370dd4ddaf
Saeid <saeed.zng@gmail.com>
2025-01-06 23:58:52 +0330

    update functions

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,7 @@ function App() {
     const ReferalIDFromUrl = window.Telegram.WebApp.initDataUnsafe.start_param;
     if (ReferalIDFromUrl) {
       setReferal_ID(ReferalIDFromUrl);
+      WebApp.showAlert(ReferalIDFromUrl);
       ConvertReferalIDToReferalEmail();
     }
   }, []);
